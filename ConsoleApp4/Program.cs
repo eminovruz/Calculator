460d4f05f2ab8f961ac194894216c0c426f7849b
Novr_wz05 <Novr_wz05@STHQ0122-04>
2022-07-29 09:00:43 +0400

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main()
+        {
+            string a_temp = "";
+            string b_temp = "";
+
+
+            while (true)
+            {
+                Console.WriteLine("1 -- +");
+                Console.WriteLine("2 -- -");
+                Console.WriteLine("3 -- *");
+                Console.WriteLine("4 -- /");
+                string Operation = Console.ReadLine();
+
+                Console.Clear();
+                Console.WriteLine("Enter a: ");
+                a_temp = Console.ReadLine();
+                Console.WriteLine("Enter b: ");
+                b_temp = Console.ReadLine();
+
+                int.TryParse(a_temp, out int a);
+                int.TryParse(b_temp, out int b);
+
+                int result = Operation switch
+                {
+                    "1" => a + b,
+                    "2" => a - b,
+                    "3" => a * b,
+                    "4" => b != 0 ? a / b : 0,
+                    _ => 0
+                };
+
+                Console.WriteLine(result != 0 ? result : "Proqramda sehvlik var") ;
+
+            }
+
+
+
+        }
+    }
+}
